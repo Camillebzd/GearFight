85e212fd40aa70871a4836acce5e55b97a67e00d
CamilleBzd <bcamille99@gmail.com>
2024-05-25 18:13:29 +0100

    [UPDATE] all the dependencies in package.json and updated the wagmi provider for raimbow wallet

--- GearFightNext/app/wagmiProviders.tsx
+++ GearFightNext/app/wagmiProviders.tsx
@@ -2,76 +2,39 @@
 
 import * as React from 'react'
 
-import {
-  RainbowKitProvider,
-  getDefaultWallets,
-  connectorsForWallets,
-} from '@rainbow-me/rainbowkit';
-import {
-  argentWallet,
-  trustWallet,
-  ledgerWallet,
-} from '@rainbow-me/rainbowkit/wallets';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-  polygon,
-  polygonMumbai
-} from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider, http } from 'wagmi'
+import { polygonAmoy } from 'viem/chains';
+import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit'
 
 const projectId = (process.env.NEXT_PUBLIC_WALLECTCONNECT_PROJECTID) as string;
 const ALCHEMY_API_KEY_MATIC = (process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_MATIC) as string;
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    // polygon,
-    polygonMumbai,
-    // ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [polygonMumbai] : []),
-  ],
-  [alchemyProvider({apiKey: ALCHEMY_API_KEY_MATIC})]
-);
+const queryClient = new QueryClient();
 
-const { wallets } = getDefaultWallets({
-  appName: 'GearFight',
-  projectId,
-  chains,
-});
-
-const demoAppInfo = {
-  appName: 'Rainbowkit Demo',
-};
-
-const connectors = connectorsForWallets([
-  ...wallets,
-  {
-    groupName: 'Other',
-    wallets: [
-      argentWallet({ projectId, chains }),
-      trustWallet({ projectId, chains }),
-      ledgerWallet({ projectId, chains }),
-    ],
+const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: projectId,
+  chains: [polygonAmoy],
+  transports: {
+    [polygonAmoy.id]: http(),
   },
-]);
+})
 
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient,
-});
-
-export function WagmiProviders({ 
-    children 
-  }: { 
-  children: React.ReactNode 
-  }) {
+export function WagmiProviders({
+  children
+}: {
+  children: React.ReactNode
+}) {
   const [mounted, setMounted] = React.useState(false);
   React.useEffect(() => setMounted(true), []);
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} appInfo={demoAppInfo} initialChain={polygonMumbai}>
-        {mounted && children}
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider initialChain={polygonAmoy}>
+          {mounted && children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
