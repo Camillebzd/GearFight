a5f7142e6aa3df455729599a043a2886b0e246f6
CamilleBzd <bcamille99@gmail.com>
2024-05-26 15:06:17 +0100

    [FIX] change mumbai for amoy and redeploy the contract

--- ContractTS/hardhat.config.ts
+++ ContractTS/hardhat.config.ts
@@ -5,9 +5,9 @@ import "dotenv/config"
 import "@nomicfoundation/hardhat-toolbox";
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const MUMBAI_RPC_URL =
-  process.env.MUMBAI_RPC_URL ||
-  "https://polygon-mumbai.g.alchemy.com/v2/your-api-key"
+const AMOY_RPC_URL =
+  process.env.AMOY_RPC_URL ||
+  "https://polygon-amoy.g.alchemy.com/v2/your-api-key"
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
   ""
@@ -17,15 +17,27 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
-    mumbai: {
-      url: MUMBAI_RPC_URL,
+    polygonAmoy: {
+      url: AMOY_RPC_URL,
       accounts: [PRIVATE_KEY],
-      chainId: 80001
+      chainId: 80002
     },
   },
   solidity: "0.8.12",
   etherscan: {
-    apiKey: POLYGONSCAN_API_KEY,
+    apiKey: {
+      polygonAmoy: POLYGONSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "polygonAmoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com"
+        },
+      }
+    ]
   },
   gasReporter: {
     enabled: false,
